90b1bb2404ea719a711ff7b9d007389a11313c25
Sahaj Khalasi <118966697+Sahaj36@users.noreply.github.com>
2023-12-26 19:30:44 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js, not browser 
+
+console.log( 3 
+    +3); // code readability should be high
+
+    console.log("Sahaj");
+
+
+    let Name = "Sahaj"
+    let age = 21
+    let isLoging = true
+    let state;
+
+    // number => 2 to power 53
+    // bigint using for big number amount
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined 
+    // symbol => unique
+
+    // object
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); // object
