f8da251a89bf54080a70e1411c416ffd0428e02f
Sahaj Khalasi <118966697+Sahaj36@users.noreply.github.com>
2023-12-28 18:43:16 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === ae datatype pan check kare 
+
+console.log("109" === 109); // false
