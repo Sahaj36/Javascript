848405849cf17b80db620c17b94a4eae63ad4a9c
Sahaj Khalasi <118966697+Sahaj36@users.noreply.github.com>
2024-01-21 19:46:40 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,48 @@
+// primetive
+
+// 7 types : number, string, null, undefind, boolearn, symbol, bigInt
+
+const number = 109
+const scoreValue = 109.36
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('109')
+const anotherId = Symbol('109')
+
+console.log(id === anotherId);
+
+const bigNumber = 4871856206720720n
+
+
+
+// non-primetive
+
+// array, object, function
+
+const heros = ["ironman", "hulk", "thor"]
+let myObj = {
+    name: "Sahaj",
+    age: 21,
+}
+
+const myFunction = function () {
+    console.log("Sahaj Khalasi");
+}
+
+console.log(typeof number);
+
+//  typeof 
+
+//  number => number
+//  string => string 
+//  null => object
+//  undefind => undefind
+//  boolearn => boolearn
+//  symbol => symbol
+//  bigInt => bigInt
+
+//  array => object
+//  object => object
