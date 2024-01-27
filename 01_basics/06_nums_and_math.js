e832509ef8e8b6d77b2c8520038d719815bc03ce
Sahaj Khalasi <118966697+Sahaj36@users.noreply.github.com>
2024-01-27 14:40:25 +0000

    numbers and math done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,25 @@
+const score = 369
+// console.log(score);
+
+const balance = new Number(109)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const anotherNumber = 109.3600
+// console.log(anotherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.6));
+console.log(Math.ceil(5.3));
+console.log(Math.floor(5.1));
+console.log(Math.min(3, 6, 9, 0));
+console.log(Math.max(0, 3, 6, 9));
+
