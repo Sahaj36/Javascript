b99d85beda3193d93d156c51f8ad7399e764f63e
Sahaj Khalasi <118966697+Sahaj36@users.noreply.github.com>
2024-01-27 14:45:48 +0000

    refinish

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -23,3 +23,11 @@ console.log(Math.floor(5.1));
 console.log(Math.min(3, 6, 9, 0));
 console.log(Math.max(0, 3, 6, 9));
 
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (min - max + 1)) + min);
