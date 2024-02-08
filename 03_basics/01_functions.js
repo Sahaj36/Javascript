70d815e9154bc3ac82cabe38f3b3323f8fa4772c
Sahaj Khalasi <118966697+Sahaj36@users.noreply.github.com>
2024-02-08 21:13:03 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,41 @@ function loginUserMessage(username = "Priyang"){
 }
 
 // console.log(loginUserMessage("Sahaj"));
-console.log(loginUserMessage("Sahaj"));
+// console.log(loginUserMessage("Sahaj"));
+
+// function calculateCartPrice(...num1) {
+//     return num1
+// }
+
+// console.log(calculateCartPrice(100, 300, 500, 109));
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 100));
+
+
+const user = {
+    username : "Sahaj",
+    fees : 1999
+}
+
+function handleObject(anyobject) {
+    console.log(`The Name is a ${anyobject.username} and fees is a ${anyobject.fees}`);
+}
+
+// handleObject(user)
+handleObject({
+    username : "Priyang",
+    fees: 999
+})
+
+const MyAarray = [200, 4000, 109, 100]
+
+function getSecondValue(getarray) {
+    return getarray[2]
+}
+
+// console.log(getSecondValue(MyAarray));
+console.log(getSecondValue([100, 500, 3600, 400]));
