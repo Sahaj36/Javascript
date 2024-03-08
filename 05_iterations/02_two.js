bae72340d2d9ecba865d533fc15e13b8ad8e16a1
Sahaj Khalasi <118966697+Sahaj36@users.noreply.github.com>
2024-03-08 19:43:21 +0000

    loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,23 @@
+let index = 0
+
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myarray = ["Ironman", "Spiderman", "Hulk"]
+
+let hero = 0
+while (hero < myarray.length) {
+    // console.log(`Hero Name is ${myarray[hero]}`);
+    hero = hero + 1
+}
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
